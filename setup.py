f33f3a387bc670c5bb0e32cf628216eae10db003
Amanj Upadhyay <amanugzp@gmail.com>
2025-03-23 17:07:11 +0530

    data ingestion and model_trainer

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirement
-    '''
-    requirement=[]
-    with open(file_path) as file_obj:
-        requirement=file_obj.readlines()
-        requirement=[req.replace("\n","") for req in requirement]
+HYPEN_E_DOT = '-e .'
 
-        if HYPEN_E_DOT in requirement:
-            requirement.remove(HYPEN_E_DOT)
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()  # Fixing this line
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
     
-    return requirement
+    return requirements
 
 setup(
-name='MLProject',
-version='0.0.1',
-author='AmanJ',
-author_email='amanugzp@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirement.txt')
-
+    name='MLProject',
+    version='0.0.1',
+    author='AmanJ',
+    author_email='amanugzp@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
